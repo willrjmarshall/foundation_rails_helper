1dc98225bcca7d515221f3192047508e7a1775c9
Will Marshall <will@willmarshall.me>
2014-02-21 13:15:42 +1100

    Giving a separate namespace to foundation form_for

--- lib/foundation_rails_helper/action_view_extension.rb
+++ lib/foundation_rails_helper/action_view_extension.rb
@@ -5,7 +5,7 @@ module ActionView
         options[:builder] ||= FoundationRailsHelper::FormBuilder
         options[:html] ||= {}
         options[:html][:class] ||= 'nice'
-        form_for_without_foundation(record, options, &block)
+        form_for(record, options, &block)
       end
 
       def fields_for_with_foundation(record_name, record_object = nil, options = {auto_labels: true}, &block)
@@ -13,11 +13,8 @@ module ActionView
         options[:html] ||= {}
         options[:html][:class] ||= 'nice'
         options[:html][:attached_labels] = options[:attached_labels]
-        fields_for_without_foundation(record_name, record_object, options, &block)
+        fields_for(record_name, record_object, options, &block)
       end
-
-      alias_method_chain :form_for, :foundation
-      alias_method_chain :fields_for, :foundation
     end
   end
 end
